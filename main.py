6025aeb5e453506d3b3cd924bb839f1e0f01ea32
camilosuarezmora <camilosuarezm2007@gmail.com>
2024-06-02 23:03:40 -0500

    termine el apartado de campers y el json de cada camper

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hola")
+#importar el txt del inicio (logo y menú principal)
+inicio = open("menu inicial.txt")
+print(inicio.read())
+inicio.close()
+
+
+
+#opcion principal
+elecc_menu_cargo = int(input(">>"))
+
+while True:
+        if elecc_menu_cargo == 4:
+                print("               SALIDA EXITOSA               ")
+                break
+        elif elecc_menu_cargo == 1:
+                from campers import camper
+        elif elecc_menu_cargo == 2:
+                print("Elegiste continuar como TRAINER")
+        elif elecc_menu_cargo == 3:
+                print("Elegiste continuar como COORDINADOR")
+
+

ed7158b3179b8c7f7c1ceda2e34f0f40b72a7799
camilosuarezmora <camilosuarezm2007@gmail.com>
2024-06-05 06:40:20 -0500

    Revert "Merge branch 'main' of https://github.com/camilosuarezmora/campus-APP"
    This reverts commit c510a9d883fc45bb966481abfca366e3f01ba504, reversing
    changes made to 1840f28c3a479a38399d8676969d23860730cb0f.

--- camper.py
+++ camper.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 import os
 
@@ -31,35 +30,6 @@ while True:
         print("=========== Usuario registrado exitosamente ===========")   
     elif elecc_mini_menu_camper == 2:
         from camper_opc2_notas import *    
-=======
-import opc1_campers_inscribirse as opc1_campers_inscribirse 
-import opc2_campers_notas as opc2_campers_notas 
-
-def funcion_ejemplo():
-    print("========== INGRESASTE COMO CAMPER ==========")
-
-    #tupla para mostrar las opciones dentro de "camper"
-    mini_menu_camper = (
-        "   Escribe 1 para inscribirte por primera vez.",
-        "   Escribe 2 para ver tus notas de modulo",
-        "   Escribe 3 para salir."
-    )
-
-    while True: 
-        for i in mini_menu_camper:
-            print(i)
-
-        elecc_mini_menu_camper = int(input(">>")) #pide al usuario la elección que hizo en el primer menú de campers
-        
-        if elecc_mini_menu_camper == len(mini_menu_camper):
-            print("               SALIDA EXITOSA               ")
-            break
-        elif elecc_mini_menu_camper == 1:
-            opc1_campers_inscribirse.funcion_inscripcion()
-            print("=========== Usuario registrado exitosamente ===========")   
-        elif elecc_mini_menu_camper == 2:
-            opc2_campers_notas.funcion_vernotas()
->>>>>>> 8f2f0377c2c3506726bf2cc9985886c885be8417
         
         
 
